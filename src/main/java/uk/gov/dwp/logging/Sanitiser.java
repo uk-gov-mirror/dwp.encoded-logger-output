38826615b2c2b281627f9b3c706d6972d1babaa0
HealthPDU <ef9823b80fb537aa52bd5de74e035b5c0e883f16>
2020-09-21 08:51:39 +0000

    new release 2.1.0

--- src/main/java/uk/gov/dwp/logging/Sanitiser.java
+++ src/main/java/uk/gov/dwp/logging/Sanitiser.java
@@ -1,12 +1,10 @@
 package uk.gov.dwp.logging;
 
-
 public class Sanitiser {
 
-    private static final String LOG_STANDARD_REGEX = "[\\u0000-\\u001f]";
-    
-    public String sanitise(String original) {
-        return original.replaceAll(LOG_STANDARD_REGEX, "");
-    }
+  private static final String LOG_STANDARD_REGEX = "[\\u0000-\\u001f]";
 
+  public String sanitise(String original) {
+    return original.replaceAll(LOG_STANDARD_REGEX, "");
+  }
 }
--- src/main/java/uk/gov/dwp/logging/logback/JsonEncoder.java
+++ src/main/java/uk/gov/dwp/logging/logback/JsonEncoder.java
@@ -6,61 +6,63 @@ import ch.qos.logback.core.Context;
 import ch.qos.logback.core.encoder.EncoderBase;
 import uk.gov.dwp.logging.LogEntryBuilderFactory;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @SuppressWarnings("squid:S1168") // allow null to be returned from byte arrays
 public class JsonEncoder extends EncoderBase<ILoggingEvent> {
 
-    private String appName = "UNKNOWN";
-    private String appVersion = "UNKNOWN";
+  private String appName = "UNKNOWN";
+  private String appVersion = "UNKNOWN";
 
-    private final LogEntryBuilderFactory entryBuilderFactory;
+  private final LogEntryBuilderFactory entryBuilderFactory;
 
-    public JsonEncoder() {
-        entryBuilderFactory = new LogEntryBuilderFactory(appName, appVersion);
-    }
+  public JsonEncoder() {
+    entryBuilderFactory = new LogEntryBuilderFactory(appName, appVersion);
+  }
 
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
+  public void setAppName(String appName) {
+    this.appName = appName;
+  }
 
-    public void setAppVersion(String appVersion) {
-        this.appVersion = appVersion;
-    }
+  public void setAppVersion(String appVersion) {
+    this.appVersion = appVersion;
+  }
 
-    @Override
-    public byte[] headerBytes() {
-        return null;
-    }
+  @Override
+  public byte[] headerBytes() {
+    return null;
+  }
 
-    @Override
-    public byte[] encode(ILoggingEvent event) {
+  @Override
+  public byte[] encode(ILoggingEvent event) {
 
-        LogEntryBuilderFactory.LogEntryBuilder builder = entryBuilderFactory.getBuilder()
-                .withAppName(appName)
-                .withAppVersion(appVersion)
-                .withLevel(event.getLevel().toString())
-                .withMessage(event.getFormattedMessage())
-                .withNameSpace(event.getLoggerName())
-                .withTimeStamp(event.getTimeStamp())
-                .withMeta("thread", event.getThreadName());
+    LogEntryBuilderFactory.LogEntryBuilder builder =
+        entryBuilderFactory
+            .getBuilder()
+            .withAppName(appName)
+            .withAppVersion(appVersion)
+            .withLevel(event.getLevel().toString())
+            .withMessage(event.getFormattedMessage())
+            .withNameSpace(event.getLoggerName())
+            .withTimeStamp(event.getTimeStamp())
+            .withMeta("thread", event.getThreadName());
 
-        Optional.ofNullable(getContext())
-                .map(Context::getCopyOfPropertyMap)
-                .ifPresent(map -> map.forEach(builder::withMeta));
+    Optional.ofNullable(getContext())
+        .map(Context::getCopyOfPropertyMap)
+        .ifPresent(map -> map.forEach(builder::withMeta));
 
-        event.getMDCPropertyMap().forEach(builder::withMeta);
+    event.getMDCPropertyMap().forEach(builder::withMeta);
 
-        Optional.ofNullable(event.getThrowableProxy())
-                .map(ThrowableProxyUtil::asString)
-                .ifPresent(p -> builder.withMeta("exception", p));
+    Optional.ofNullable(event.getThrowableProxy())
+        .map(ThrowableProxyUtil::asString)
+        .ifPresent(p -> builder.withMeta("exception", p));
 
-        return builder.build().getBytes();
-    }
+    return builder.build().getBytes(StandardCharsets.UTF_8);
+  }
 
-    @Override
-    public byte[] footerBytes() {
-        return null;
-    }
-
+  @Override
+  public byte[] footerBytes() {
+    return null;
+  }
 }
