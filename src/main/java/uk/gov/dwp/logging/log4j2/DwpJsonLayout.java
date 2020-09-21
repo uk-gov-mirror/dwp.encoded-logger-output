52aa040639e5600b7715a93f97535cb3f0aed180
HealthPDU <health.platform@engineering.digital.dwp.gov.uk>
2020-09-21 08:51:39 +0000

    new release 2.1.0

--- src/main/java/uk/gov/dwp/logging/log4j2/DwpJsonLayout.java
+++ src/main/java/uk/gov/dwp/logging/log4j2/DwpJsonLayout.java
@@ -11,67 +11,43 @@ import uk.gov.dwp.logging.LogEntryBuilderFactory;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
-@Plugin(
-        name = "DwpJsonLayout", 
-        category = "Core", 
-        elementType = "layout", 
-        printObject = true
-)
+@Plugin(name = "DwpJsonLayout", category = "Core", elementType = "layout", printObject = true)
 public final class DwpJsonLayout extends AbstractStringLayout {
 
-    private final LogEntryBuilderFactory logEntryBuilderFactory;
-    
-    
-    public DwpJsonLayout(
-            Charset charset,
-            String appName,
-            String version
-    ) {
-        super(charset);
-        logEntryBuilderFactory = new LogEntryBuilderFactory(appName, version);
-    }
-    
-    @Override
-    public String toSerializable(LogEvent le) {
-        
-        LogEntryBuilderFactory.LogEntryBuilder builder = logEntryBuilderFactory.getBuilder()
-                .withLevel(le.getLevel().name())
-                .withMessage(le.getMessage().getFormattedMessage())
-                .withNameSpace(le.getLoggerName())
-                .withTimeStamp(le.getNanoTime())
-                .withMeta("thread", ""+le.getThreadId());
-                
-        
-        Optional.ofNullable(le.getThrown())
-                .ifPresent(
-                        thrown -> builder.withMeta(
-                                "exception", 
-                                String.join(
-                                        "\n", 
-                                        Throwables.toStringList(thrown)
-                                                .toArray(new String[0])
-                                )        
-                        )
-                );
-        
-        return builder.build();
-    }
-    
-    @PluginFactory
-    public static DwpJsonLayout createLayout(
-            @PluginAttribute(
-                    value = "charset", 
-                    defaultString = "UTF-8"
-            ) Charset charset,
-            @PluginAttribute(
-                    value = "app_name"
-            ) String appName,
-            @PluginAttribute(
-                    value = "app_version"
-            ) String version
-    ) {
-        return new DwpJsonLayout(charset, appName, version);
-    }
-    
-}
+  private final LogEntryBuilderFactory logEntryBuilderFactory;
 
+  public DwpJsonLayout(Charset charset, String appName, String version) {
+    super(charset);
+    logEntryBuilderFactory = new LogEntryBuilderFactory(appName, version);
+  }
+
+  @Override
+  public String toSerializable(LogEvent le) {
+
+    LogEntryBuilderFactory.LogEntryBuilder builder =
+        logEntryBuilderFactory
+            .getBuilder()
+            .withLevel(le.getLevel().name())
+            .withMessage(le.getMessage().getFormattedMessage())
+            .withNameSpace(le.getLoggerName())
+            .withTimeStamp(le.getNanoTime())
+            .withMeta("thread", "" + le.getThreadId());
+
+    Optional.ofNullable(le.getThrown())
+        .ifPresent(
+            thrown ->
+                builder.withMeta(
+                    "exception",
+                    String.join("\n", Throwables.toStringList(thrown).toArray(new String[0]))));
+
+    return builder.build();
+  }
+
+  @PluginFactory
+  public static DwpJsonLayout createLayout(
+      @PluginAttribute(value = "charset", defaultString = "UTF-8") Charset charset,
+      @PluginAttribute(value = "app_name") String appName,
+      @PluginAttribute(value = "app_version") String version) {
+    return new DwpJsonLayout(charset, appName, version);
+  }
+}
